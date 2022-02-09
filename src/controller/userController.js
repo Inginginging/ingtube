fef7b67dc416700eb64e0e2abf4a83e729ad93b1
ing928 <kik928@nate.com>
2022-02-09 20:26:13 +0900

    fixing bugs

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,207 +1,231 @@
 import User from "../models/User";
 import Video from "../models/Video";
 import bcrypt from "bcrypt";
-import fetch from "node-fetch" //node 에서는 fetch 이해를 못하므로 node-fetch 설치 후 import
+import fetch from "node-fetch"; //node 에서는 fetch 이해를 못하므로 node-fetch 설치 후 import
 
-export const getJoin = (req,res) => res.render("user/join", {pageTitle: "Join"});
+export const getJoin = (req, res) =>
+  res.render("user/join", { pageTitle: "Join" });
 
-export const postJoin = async(req,res) =>{
-    const {name, username, email, password, password2, location} = req.body;
-    if(password != password2){ //password의 확인 여부 확인
-        req.flash("error", "The password Confirmation failed");
-        return res.status(400).render("user/join", {pageTitle: "Join"});
-    }
-    const exists = await User.exists({ $or: [ {username}, {email} ]} );//User db안에 중복되는 username과 email의 존재 여부 확인 
-    if(exists){
-        req.flash("error", "This Username/Email is already taken");
-        return res.status(400).render("user/join", {pageTitle: "Join"});
-    }
-    try{ //그외의 오류들을 잡기 위해 예외 처리 구문 사용.
-        await User.create({
-            name, //===name:name
-            username,
-            email,
-            password,
-            location,
-        });
-        return res.redirect("/login")
-    }catch(error){
-        req.flash("error", error._message);
-        return res.render("user/join", {pageTitle: "Join"});
-    }
+export const postJoin = async (req, res) => {
+  const { name, username, email, password, password2, location } = req.body;
+  if (password != password2) {
+    //password의 확인 여부 확인
+    req.flash("error", "The password Confirmation failed");
+    return res.status(400).render("user/join", { pageTitle: "Join" });
+  }
+  const exists = await User.exists({ $or: [{ username }, { email }] }); //User db안에 중복되는 username과 email의 존재 여부 확인
+  if (exists) {
+    req.flash("error", "This Username/Email is already taken");
+    return res.status(400).render("user/join", { pageTitle: "Join" });
+  }
+  try {
+    //그외의 오류들을 잡기 위해 예외 처리 구문 사용.
+    await User.create({
+      name, //===name:name
+      username,
+      email,
+      password,
+      location,
+    });
+    return res.redirect("/login");
+  } catch (error) {
+    req.flash("error", error._message);
+    return res.render("user/join", { pageTitle: "Join" });
+  }
 };
 
-export const getLogin = (req,res) =>res.render("user/login", {pageTitle: "Login"});
+export const getLogin = (req, res) =>
+  res.render("user/login", { pageTitle: "Login" });
 
-export const postLogin = async(req, res) =>{
-    const {username, password} = req.body;
-    const user = await User.findOne({username, socialOnly:false}) //db에서 req.body의 username과 db안의 username이 같은것이 있는지 확인
-    if(!user){ //username이 db안에 등록되어 있지 않을때.
-        req.flash("error", "No such Username");
-        return res.status(400).render("user/login", {pageTitle: "Login"});
-    }
-    const ok = await bcrypt.compare(password, user.password); //rep.body로 부터 받은 password와 db안의 password가 같은지 확인
-    if(!ok){
-        req.flash("error", "Wrong password");
-        return res.status(400).render("user/login", {pageTitle: "Login"});
-    }
-    req.session.loggedIn = true; //req.session 객체에 loggedIn 프로퍼티 추가 후 값을. true로 설정
-    req.session.user = user; //req.session 객체에 로그인하려는 user의 정보 프로퍼티로 추가.
-    req.flash("info", "Welcome");
-    return res.redirect("/")
-}
-
-export const startGithubLogin = (req,res) => {
-    const baseUrl = "https://github.com/login/oauth/authorize";
-    const config = {
-        client_id: process.env.GH_CLIENT,
-        allow_signup: false, //github 회원가입은 못하게함. github 회원가입은 github 홈페이지로 가서 하시오.
-        scope: 'read:user user:email',
-    }
-    const params = new URLSearchParams(config).toString();
-    const finalUrl = `${baseUrl}?${params}`; //redirect할 chlwhd url
-    return res.redirect(finalUrl);
-}
-
-export const finishGithubLogin = async(req,res) =>{
-    const baseUrl = "https://github.com/login/oauth/access_token";
-    const config = {
-        client_id: process.env.GH_CLIENT,
-        client_secret: process.env.GH_SECRET,
-        code: req.query.code, //code: scope에 적은 정보에 접근할 수 있게 github에서 제공 해주는 code
-    };
-    const params = new URLSearchParams(config).toString();
-    const finalUrl = `${baseUrl}?${params}`;
-    const tokenRequest = await(await fetch(finalUrl,{
-        method: "POST",
-        headers: {
-            Accept: "application/json"
-            },
-        })
-    ).json(); //finalUrl에 post함으로써 json형식의 객체 받음. 안에는 token, token type, scope이 있음.
-    if ("access_token" in tokenRequest) {
-        const { access_token } = tokenRequest; 
-        const apiUrl = "https://api.github.com";
-        const userData = await(
-            await fetch(`${apiUrl}/user`,{
-                headers: {
-                    Authorization: `token ${access_token}`, //access_token: code를 access_token으로 변경시킨것
-                    }
-                })
-            ).json();
-        const emailData = await(
-            await fetch(`${apiUrl}/user/emails`,{
-                headers: {
-                    Authorization: `token ${access_token}`, 
-                    }
-                })
-            ).json();
-        const emailObj = emailData.find(
-            (email) => email.primary === true && email.verified === true
-        );
-        if(!emailObj){ //email은 필수 조건이므로. 이메일이 없으면 로그인할수 없음.
-            req.flash("error", "You don't have any Email on Github");
-            return res.redirect("/login") 
-        }
-        let user = await User.findOne({emial: emailObj.email}) //db에서 기존에 등록된 email과 github login email이 같은것이 있는지 확인
-        if(!user){ //기존 사용자가 아닐때
-            user = await User.create({
-                name: userData.name, //===name:name
-                username: userData.login,
-                socialOnly: true,
-                avatarUrl: userData.avatar_url, 
-                email: emailObj.email,
-                password: "",
-                location: userData.location,
-                })
-            };  
-        req.session.loggedIn = true; 
-        req.session.user = user; 
-        return res.redirect("/")
-    }else{
-        req.flash("error","Cannot Login with Github account")
-        return res.redirect("/login")
-    }
-}
-
-export const logout = (req,res) => {
-    req.session.destroy();
-    res.redirect("/")
+export const postLogin = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await User.findOne({ username, socialOnly: false }); //db에서 req.body의 username과 db안의 username이 같은것이 있는지 확인
+  if (!user) {
+    //username이 db안에 등록되어 있지 않을때.
+    req.flash("error", "No such Username");
+    return res.status(400).render("user/login", { pageTitle: "Login" });
+  }
+  const ok = await bcrypt.compare(password, user.password); //rep.body로 부터 받은 password와 db안의 password가 같은지 확인
+  if (!ok) {
+    req.flash("error", "Wrong password");
+    return res.status(400).render("user/login", { pageTitle: "Login" });
+  }
+  req.session.loggedIn = true; //req.session 객체에 loggedIn 프로퍼티 추가 후 값을. true로 설정
+  req.session.user = user; //req.session 객체에 로그인하려는 user의 정보 프로퍼티로 추가.
+  req.flash("info", "Welcome");
+  return res.redirect("/");
 };
 
-export const getEdit = (req,res) => res.render("user/edit-profile", {pageTitle: "Edit Profile"});
-
-export const postEdit = async (req,res) =>{
-    const { _id, avatarUrl} = req.session.user; //login했을때 req.session안에 user정보를 집어넣어 놧음.
-    const { name, username, email, location } = req.body;
-    const user = await User.findById(_id); //edit 하기 전의 user
-    const file = req.file; //router에서 uploadfile middleware를 사용함으로서 req.file 사용 가능.
-    if(username != user.username){ //username이 변경되었을때
-        const exists = await User.findOne({username}) //db에 중복되는 username이 있는지 확인
-        if(exists){
-            req.flash("error","The Username already taken");
-            return res.status(400).render("user/edit-profile", {pageTitle: "Edit Profile"});
-        }
-    }
-    if(email != user.email){ //email이 변경되었을때
-        const exists = await User.findOne({email}) //db에 중복되는 emial이 있는지 확인
-        if(exists){
-            req.flash("error","Someone alreay using the Email");
-            return res.status(400).render("user/edit-profile", {pageTitle: "Edit Profile"});
-        }
-    }
-    const isHeroku = process.env.NODE_ENV === "production";
-    const updatedUser = await User.findByIdAndUpdate( _id, {
-        avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl, //file이 변경 되엇으면 file경로의 file로 avatarUrl 바꿈. 아니면 기존의 avatarUrl유지.
-        name,
-        username,
-        email,
-        location,
-    }, {new: true}) //new:true를 해주므로서 update상태 유지(?)
-    req.session.user = updatedUser; //req.session의 user를 updateduser로 교환
-    return res.redirect("/users/edit");
-}
-
-export const getChangePw = (req,res) => {
-    if (req.session.user.socialOnly === true) {
-      req.flash("error", "The Social User Cannot change the Password");
-      return res.redirect("/");
-    }
-    return res.render("user/changePw", { pageTitle: "Change Password" });
+export const startGithubLogin = (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/authorize";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    allow_signup: false, //github 회원가입은 못하게함. github 회원가입은 github 홈페이지로 가서 하시오.
+    scope: "read:user user:email",
   };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`; //redirect할 chlwhd url
+  return res.redirect(finalUrl);
+};
 
-export const postChangePw = async(req,res) => {
-    const { _id } = req.session.user;
-    const { oldPw, newPw, newPw1 } = req.body;
-    const user = await User.findById(_id);
-    const ok = await bcrypt.compare(oldPw , user.password);
-    if(!ok){ //db에 저장된 pw와 form에 기입한 pw가  다를떄
-        req.flash("error", "The current Password is Incorrect");
-        return res.status(400).render("user/changePw", {pageTitle: "Change Password",});
-    }
-    if(newPw != newPw1){
-        req.flash("error", "Password Confirmation Failed");
-        return res.status(400).render("user/changePw", {pageTitle: "Change Password"});
-    }
-    user.password = newPw;
-    await user.save(); //save 하면 model에서 설정한 pre middleware가 작동하여 hash해줌
-    req.flash("info", "Password updated");
-    return res.redirect("/users/logout")
-}
-
-export const see = async(req,res) =>{
-    const {id} = req.params; //profile page는 모두가 볼 수 있으므로 user session에서 받아오는것이 아니라 url에서 받아와야됨.
-    const user = await User.findById(id).populate({
-        path:"videos",
-        populate: {
-            path: "owner",
-            model: "User",
+export const finishGithubLogin = async (req, res) => {
+  const baseUrl = "https://github.com/login/oauth/access_token";
+  const config = {
+    client_id: process.env.GH_CLIENT,
+    client_secret: process.env.GH_SECRET,
+    code: req.query.code, //code: scope에 적은 정보에 접근할 수 있게 github에서 제공 해주는 code
+  };
+  const params = new URLSearchParams(config).toString();
+  const finalUrl = `${baseUrl}?${params}`;
+  const tokenRequest = await (
+    await fetch(finalUrl, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+      },
+    })
+  ).json(); //finalUrl에 post함으로써 json형식의 객체 받음. 안에는 token, token type, scope이 있음.
+  if ("access_token" in tokenRequest) {
+    const { access_token } = tokenRequest;
+    const apiUrl = "https://api.github.com";
+    const userData = await (
+      await fetch(`${apiUrl}/user`, {
+        headers: {
+          Authorization: `token ${access_token}`, //access_token: code를 access_token으로 변경시킨것
         },
-    });//mongoose의 populate메서드는 model의 videos안에 있는 ref로 부터 video'객체'정보를 찾아옴.
-    if(!user){
-        req.flash("error", "There is No such User");
-        return res.status(404).render("404", {pageTitle: "Cannot find User"});
+      })
+    ).json();
+    const emailData = await (
+      await fetch(`${apiUrl}/user/emails`, {
+        headers: {
+          Authorization: `token ${access_token}`,
+        },
+      })
+    ).json();
+    const emailObj = emailData.find(
+      (email) => email.primary === true && email.verified === true
+    );
+    if (!emailObj) {
+      //email은 필수 조건이므로. 이메일이 없으면 로그인할수 없음.
+      req.flash("error", "You don't have any Email on Github");
+      return res.redirect("/login");
     }
-    //const videos = await Video.find({owner: user._id});
-    return res.render("user/profile", {pageTitle: user.username, user,});
-}
+    let user = await User.findOne({ emial: emailObj.email }); //db에서 기존에 등록된 email과 github login email이 같은것이 있는지 확인
+    if (!user) {
+      //기존 사용자가 아닐때
+      user = await User.create({
+        name: userData.name, //===name:name
+        username: userData.login,
+        socialOnly: true,
+        avatarUrl: userData.avatar_url,
+        email: emailObj.email,
+        password: "",
+        location: userData.location,
+      });
+    }
+    req.session.loggedIn = true;
+    req.session.user = user;
+    return res.redirect("/");
+  } else {
+    req.flash("error", "Cannot Login with Github account");
+    return res.redirect("/login");
+  }
+};
+
+export const logout = (req, res) => {
+  req.session.destroy();
+  res.redirect("/");
+};
+
+export const getEdit = (req, res) =>
+  res.render("user/edit-profile", { pageTitle: "Edit Profile" });
+
+export const postEdit = async (req, res) => {
+  const { _id, avatarUrl } = req.session.user; //login했을때 req.session안에 user정보를 집어넣어 놧음.
+  const { name, username, email, location } = req.body;
+  const user = await User.findById(_id); //edit 하기 전의 user
+  const file = req.file; //router에서 uploadfile middleware를 사용함으로서 req.file 사용 가능.
+  if (username != user.username) {
+    //username이 변경되었을때
+    const exists = await User.findOne({ username }); //db에 중복되는 username이 있는지 확인
+    if (exists) {
+      req.flash("error", "The Username already taken");
+      return res
+        .status(400)
+        .render("user/edit-profile", { pageTitle: "Edit Profile" });
+    }
+  }
+  if (email != user.email) {
+    //email이 변경되었을때
+    const exists = await User.findOne({ email }); //db에 중복되는 emial이 있는지 확인
+    if (exists) {
+      req.flash("error", "Someone alreay using the Email");
+      return res
+        .status(400)
+        .render("user/edit-profile", { pageTitle: "Edit Profile" });
+    }
+  }
+  const isHeroku = process.env.NODE_ENV === "production";
+  const updatedUser = await User.findByIdAndUpdate(
+    _id,
+    {
+      avatarUrl: file ? (isHeroku ? file.location : file.path) : avatarUrl, //file이 변경 되엇으면 file경로의 file로 avatarUrl 바꿈. 아니면 기존의 avatarUrl유지.
+      name,
+      username,
+      email,
+      location,
+    },
+    { new: true }
+  ); //new:true를 해주므로서 update상태 유지(?)
+  req.session.user = updatedUser; //req.session의 user를 updateduser로 교환
+  return res.redirect(`/users/${user._id}`);
+};
+
+export const getChangePw = (req, res) => {
+  if (req.session.user.socialOnly === true) {
+    req.flash("error", "The Social User Cannot change the Password");
+    return res.redirect("/");
+  }
+  return res.render("user/changePw", { pageTitle: "Change Password" });
+};
+
+export const postChangePw = async (req, res) => {
+  const { _id } = req.session.user;
+  const { oldPw, newPw, newPw1 } = req.body;
+  const user = await User.findById(_id);
+  const ok = await bcrypt.compare(oldPw, user.password);
+  if (!ok) {
+    //db에 저장된 pw와 form에 기입한 pw가  다를떄
+    req.flash("error", "The current Password is Incorrect");
+    return res
+      .status(400)
+      .render("user/changePw", { pageTitle: "Change Password" });
+  }
+  if (newPw != newPw1) {
+    req.flash("error", "Password Confirmation Failed");
+    return res
+      .status(400)
+      .render("user/changePw", { pageTitle: "Change Password" });
+  }
+  user.password = newPw;
+  await user.save(); //save 하면 model에서 설정한 pre middleware가 작동하여 hash해줌
+  req.flash("info", "Password updated");
+  return res.redirect("/users/logout");
+};
+
+export const see = async (req, res) => {
+  const { id } = req.params; //profile page는 모두가 볼 수 있으므로 user session에서 받아오는것이 아니라 url에서 받아와야됨.
+  const user = await User.findById(id).populate({
+    path: "videos",
+    populate: {
+      path: "owner",
+      model: "User",
+    },
+  }); //mongoose의 populate메서드는 model의 videos안에 있는 ref로 부터 video'객체'정보를 찾아옴.
+  if (!user) {
+    req.flash("error", "There is No such User");
+    return res.status(404).render("404", { pageTitle: "Cannot find User" });
+  }
+  //const videos = await Video.find({owner: user._id});
+  return res.render("user/profile", { pageTitle: user.username, user });
+};
